421ad581319a86c32bb4772de1b5bbfa63bc66a7
favour igbokwe <igbokwefc1@gmail.com>
2024-07-04 21:26:48 +0100

    font adjustment

--- components/recommended/header.js
+++ components/recommended/header.js
@@ -9,7 +9,7 @@ export default function RecomHeader({ title, color, seeAll }) {
     <div
       className={`${color} p-3 rounded-t-lg text-xl text-white  flex justify-between`}
     >
-      <h2 className="upp uppercase lg:mx-auto">{title}</h2>
+      <h2 className=" lg:mx-auto">{title}</h2>
       <a href={`/allProducts/${title}`} className="text-sm cursor-pointer">
         see all {">"}
       </a>
